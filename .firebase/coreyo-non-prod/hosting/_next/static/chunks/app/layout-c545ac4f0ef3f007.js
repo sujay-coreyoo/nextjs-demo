245c93c35c9cc37be17900bb5c43d4aed58ac6f1
Sujay Apale <92621122+sujay-coreyoo@users.noreply.github.com>
2023-04-04 18:34:04 +0530

    second commit

---
+++ .firebase/coreyo-non-prod/hosting/_next/static/chunks/app/layout-c545ac4f0ef3f007.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{9074:function(n,e,u){Promise.resolve().then(u.bind(u,4026))},4026:function(n,e,u){"use strict";u.r(e),e.default="bebd58b59507"}},function(n){n.O(0,[667,961,744],function(){return n(n.s=9074)}),_N_E=n.O()}]);
